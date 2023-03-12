159389a5d488af83424ecd2aecb6fcb6c6964427
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-12 22:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+function myFunction() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW Row";
+  cell2.innerHTML = "NEW Row";
+}
