49d21fc4be889199f5fb55ffb21bf4acd83a13ee
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-12 22:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function myFunction() {
   var row = table.insertRow(0);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW Row";
-  cell2.innerHTML = "NEW Row";
+  cell1.innerHTML = "0";
+  cell2.innerHTML = "0";
+}
+ function cart(){
+    var Name = document.getElementById('item').value;
+  var heading = document.getElementById('price').value;
+    document.getElementById('total').innerHTML += '<br>' +heading + Name ;
 }
